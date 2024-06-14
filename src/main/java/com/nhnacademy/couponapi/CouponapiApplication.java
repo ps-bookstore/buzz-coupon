b2c0ce8f1ff55615a0c107a4cae8572c3cdc6fb3
park <qmzo553@gmail.com>
2024-06-14 17:14:14 +0900

    init

---
+++ src/main/java/com/nhnacademy/couponapi/CouponapiApplication.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.couponapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouponapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CouponapiApplication.class, args);
+    }
+
+}
