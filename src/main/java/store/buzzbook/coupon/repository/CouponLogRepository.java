90c175fca2ca38bed613fa1fa91610aa6e64050d
park <qmzo553@gmail.com>
2024-06-25 10:49:10 +0900

    :bug: fix: coupon log

--- src/main/java/store/buzzbook/coupon/repository/CouponLogRepository.java
+++ src/main/java/store/buzzbook/coupon/repository/CouponLogRepository.java
@@ -4,12 +4,15 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import jakarta.validation.constraints.NotNull;
 import store.buzzbook.coupon.dto.couponlog.CouponLogResponse;
 import store.buzzbook.coupon.entity.CouponLog;
+import store.buzzbook.coupon.entity.constant.CouponStatus;
 
 public interface CouponLogRepository extends JpaRepository<CouponLog, Long> {
 
-	Page<CouponLogResponse> findAllByUserIdAndStatus(Long userId, String status, Pageable pageable);
+	Page<CouponLogResponse> findAllByUserIdAndStatus(@NotNull long userId, @NotNull CouponStatus status,
+		Pageable pageable);
 
 	boolean existsByCouponPolicyIdAndUserId(int couponPolicyId, long userId);
 
